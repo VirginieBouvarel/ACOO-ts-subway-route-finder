bdc72d776526264799c295f472b6571ad28ab046
Virginie <vb@ikimo9.com>
2024-09-05 14:51:42 +0200

    adding test module classe

---
+++ src/tests/LoadTester.ts
@@ -0,0 +1,37 @@
+import { SubwayLoader } from "../SubwayLoader";
+
+export class LoadTester {
+  async main(): Promise<void> {
+    try {
+      const loader = new SubwayLoader();
+
+      const objectVille = await loader.loadFromFile('./network.txt');
+
+      console.log('Test des stations...');
+      if (
+        objectVille.hasStation("Allée de Non duplication") &&
+        objectVille.hasStation("Station Météo") &&
+        objectVille.hasStation("Les planches à gogo")
+      ) {
+        console.log('...Test des stations réussi.');
+      } else {
+        console.log('...Test des stations échoué.');
+      }
+
+      console.log('\nTest des liaisons...');
+      if (
+        objectVille.hasConnection("Allée de Non duplication", "Théâtre Tête la première", "Ligne Meyer") &&
+        objectVille.hasConnection("Station Météo", "Rocade XHTML", "Ligne Wirfs-Brock") &&
+        objectVille.hasConnection("Théâtre Tête la première", "Cercle Infini", "Ligne Rumbaugh")
+      ) {
+        console.log('...Test des liaisons réussi.');
+      } else {
+        console.log('...Test des liaisons échoué');
+        console.log(objectVille);
+
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  }
+}
